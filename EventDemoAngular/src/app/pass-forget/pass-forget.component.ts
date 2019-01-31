94a48d2371e10d3d4e59030b8e150f2055280c4c
Ahmad Al Howimel <Alfahd.dev@gmail.com>
2019-01-31 10:43:40 +0300

    version Beta 2 Need to active Notification service

--- EventDemoAngular/src/app/pass-forget/pass-forget.component.ts
+++ EventDemoAngular/src/app/pass-forget/pass-forget.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AuthenticationService} from '../auth/authentication.service';
 import {UserService} from '../user/user.service';
 
 @Component({
-  selector: 'app-pass-forget',
-  templateUrl: './pass-forget.component.html',
-  styleUrls: ['./pass-forget.component.css']
+    selector: 'app-pass-forget',
+    templateUrl: './pass-forget.component.html',
+    styleUrls: ['./pass-forget.component.css']
 })
 export class PassForgetComponent implements OnInit {
 
     loginForm: FormGroup;
     error;
+    loading;
 
     constructor(
         private formBuilder: FormBuilder,
         private route: ActivatedRoute,
         private router: Router,
-        private userService: UserService,
-        private authenticationService: AuthenticationService) {
+        private userService: UserService) {
     }
 
     ngOnInit() {
@@ -29,6 +29,12 @@ export class PassForgetComponent implements OnInit {
     }
 
     onSubmit() {
-      this.userService.resetRequest(this.loginForm.controls.username.value,window.location.origin+'/reset/').subscribe(value => this.router.navigateByUrl("/login"),error1 => this.error=error1);
+        this.loading = true;
+        this.userService.resetRequest(this.loginForm.controls.username.value, window.location.origin + '/reset/')
+            .subscribe(value => this.router.navigateByUrl("/login"),
+                error1 => {
+                    this.loading = false;
+                    this.error = error1;
+                });
     }
 }
